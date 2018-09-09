9ad2e48eca29a0bfff62e078a0a35b07ff660ab3
HjalmarThulstrup <hjalmar_thulstrup@hotmail.com>
2018-09-09 12:01:19 +0200

    page 2 is donzo

--- js/page2.js
+++ js/page2.js
@@ -7,6 +7,17 @@ const theTimer = document.querySelector(".timer");
 var timer = [0,0,0,0];
 var interval;
 var timerRunning = false;
+var texts = ["I don't want to talk to you no more, you empty-headed animal food trough wiper! I fart in your general direction! Your mother was a hamster and your father smelt of elderberries!",
+ "Society is commonly too cheap. We meet at very short intervals, not having had time to acquire any new value for each other. We meet at meals three times a day, and give each other a new taste of that old musty cheese that we are.",
+"Don't tell me it's not worth trying for. You can't tell me it's not worth dying for. You know it's true. Everything I do, I do it for you.",
+"G.I. Joel's peers can question his ability, world championship or not, and laugh at his physical tics, but it's hard not to admire his quirky devotion.",
+"There are places I'll remember all my life, though some have changed. Some forever, not for better. Some have gone and some remain. All these places have their moments with lovers and friends I still can recall.",
+"The whole point was to find a way to practice nuclear war without destroying ourselves. To get the computers to learn from mistakes we couldn't afford to make. Except I never could get Joshua to learn the most important lesson.",
+"I'd ask you about love, you'd probably quote me a sonnet. But you've never looked at a woman and been totally vulnerable. Known someone that could level you with her eyes, feeling like God put an angel on earth just for you.",
+"Messes of men in farmer poverty; not much for monks but we pretend to be. Share a silent meal and a pot of chamomile, gypsies like us should be stamped in solidarity. Hold you in my fond but distant memory while waiting for the Mother Hen to gather me who regretfully wrote.",
+"It was hard to toss things I had once thought were valuable enough to spend money on and just as hard to separate myself from worn and ragged clothing I had for sentimental reasons. Once I'd passed through the first few tough decisions, though, the momentum had been built and it was a breeze.",
+"It looks like your little adventure has reached its penultimate chapter. I suppose that means it's time for me to go look for real work... Whoa! I can't believe I just said that! No, seriously... I gotta find that treasure..."
+];
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time){
     if(time <= 9){
@@ -63,9 +74,17 @@ function reset(){
     testArea.value = "";
     theTimer.innerHTML = "00:00:00";
     testWrapper.style.borderColor = "grey";
+    newText();
 }
 
 // Event listeners for keyboard input and the reset button:
 testArea.addEventListener("keypress", start, false);
 testArea.addEventListener("keyup", spellCheck, false);
 resetButton.addEventListener("click", reset, false);
+
+// Random text chooser
+function newText(){
+    let rnd = Math.floor(Math.random() * (9 - 0 + 1));
+
+    document.querySelector("#origin-text p").innerHTML = texts[rnd];
+}
